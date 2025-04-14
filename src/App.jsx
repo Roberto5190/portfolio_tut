12cbd3e437db356e6fadc6ba83b21f95fe8feca0
Roberto <rfernandez5190@esdmadrid.es>
2025-04-14 02:36:48 +0200

    init proyect

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <div>
+          <h1 className="text-4xl font-bold">Welcome to my 3D portfolio</h1>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
