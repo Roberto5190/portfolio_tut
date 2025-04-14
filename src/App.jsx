e565d64fa1b2116a0270a19950e98302831ca800
Roberto <rfernandez5190@esdmadrid.es>
2025-04-14 03:26:54 +0200

    added assets folder and constants from tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <main>
+      <main>i
         <div className="flex-center flex-col gap-4">
           <h1 className="text-4xl font-bold ">Welcome to my 3D portfolio</h1>
           <p className="text-lg text-white-50">This is a portfolio of my 3D projects</p>
