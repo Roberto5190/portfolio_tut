14e8443ab06ddaa69e48645e98fbbe6c56cb7ce7
Roberto <rfernandez5190@esdmadrid.es>
2025-04-14 16:16:24 +0200

    added hero text

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-
+import Hero from './sections/Hero'
 
 function App() {
 
   return (
     <>
-      <main>i
-        <div className="flex-center flex-col gap-4">
-          <h1 className="text-4xl font-bold ">Welcome to my 3D portfolio</h1>
-          <p className="text-lg text-white-50">This is a portfolio of my 3D projects</p>
-        </div>
-      </main>
+      <Hero />
     </>
   )
 }
