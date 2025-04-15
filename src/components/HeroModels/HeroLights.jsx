9aefc51d3fe6c724ce5caa08f22e2c20d2c80655
Roberto <rfernandez5190@esdmadrid.es>
2025-04-15 14:57:17 +0200

    fixed hero lights on mobile

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import * as THREE from 'three';
+import { useMediaQuery } from 'react-responsive'
 
 
 const HeroLights = () => {
+
+  const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
   return (
     <>
 
         <spotLight
-            position={[5, 35, 24]}
+            position={isMobile ? [5, 35, 24] : [5, 60, 40]}
             intensity={20000}
             penumbra={.2}
             color={"#ff5d00"}
