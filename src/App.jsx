6a1052e100f1f963b1d500ea8f1603c7f7438ef6
Roberto <rfernandez5190@esdmadrid.es>
2025-04-16 16:16:29 +0200

    added ShowCaseSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from './sections/Hero'
+import ShowCaseSection from './sections/ShowCaseSection'
 
 function App() {
 
   return (
     <>
       <Hero />
+      <ShowCaseSection />
     </>
   )
 }
