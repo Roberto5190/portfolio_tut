6fee5885dd02f1aac49d0180487cab3f401ed705
Roberto <rfernandez5190@esdmadrid.es>
2025-04-16 15:17:06 +0200

    added animatedCounter component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,6 +4,8 @@ import Button from "../components/Button";
 import HeroExperience from "../components/HeroModels/HeroExperience";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import AnimatedCounter from "../components/AnimatedCounter";
+
 
 const Hero = () => {
 
@@ -76,6 +78,9 @@ const Hero = () => {
           </div>
         </figure>
       </div>
+
+      {/* ANIMATED COUNTER */}
+      <AnimatedCounter />
     </section>
   );
 };
