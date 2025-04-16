2f6733095b76c72768c1a100a8d828a364593551
Roberto <rfernandez5190@esdmadrid.es>
2025-04-17 01:07:03 +0200

    added animations to experience section

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -2,8 +2,67 @@ import React from "react";
 import TitleHeader from "../components/TitleHeader";
 import { expCards } from "../../constants";
 import GlowCard from "../components/GlowCard";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
 
 const ExperienceSection = () => {
+
+    Animation
+    useGSAP(() => {
+        gsap.utils.toArray('.timeline-card').forEach((card) => {
+            gsap.from(card, 
+                {
+                    xPercent: -100,
+                    opacity: 0,
+                    transformOrigin: 'left left',
+                    duration: 1,
+                    ease: 'power2.inOut',
+                    scrollTrigger: {
+                        trigger: card,
+                        start: 'top 80%'
+                    }
+                }
+            )
+            
+        })
+
+        gsap.to('.timeline',
+            {
+                transformOrigin: 'bottom bottom',
+                ease: 'power2.inOut',
+                scrollTrigger: {
+                    trigger: '.timeline',
+                    start: 'top center',
+                    end: '70% center',
+                    onUpdate: () => {
+                        gsap.to('.timeline', {
+                            scaleY: 1 - self.progress
+                        })
+                    }
+                }
+            }
+        )
+
+        gsap.utils.toArray('.expText').forEach((text) => {
+            gsap.from(text, 
+                {
+                    xPercent: 0,
+                    opacity: 0,
+                    duration: 1,
+                    ease: 'power2.inOut',
+                    scrollTrigger: {
+                        trigger: text,
+                        start: 'top 60%'
+                    }
+                }
+            )
+            
+        })
+    }, [])
+
     return (
         <section
             id="experience"
@@ -47,7 +106,7 @@ const ExperienceSection = () => {
                                                 <h1 className="font-semibold text-3xl">{card.title}</h1>
                                                 <p className="my-5 text-white-50">📆 {card.date}</p>
                                                 <p className="text-[#839cb5] italic">Responsibilities</p>
-                                                <ul className="list-disc list-inside">
+                                                <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
                                                     {card.responsibilities.map((responsibility) => (
                                                         <li key={responsibility}>{responsibility}</li>
                                                     ))}
