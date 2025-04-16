b713203d3bc9c5c70ed1dd2dbd39cbc743a3ea9e
Roberto <rfernandez5190@esdmadrid.es>
2025-04-17 00:23:03 +0200

    added experience

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,34 +1,71 @@
-import React from 'react'
-import TitleHeader from '../components/TitleHeader'
-import { expCards } from '../../constants'
-import GlowCard from '../components/GlowCard'
+import React from "react";
+import TitleHeader from "../components/TitleHeader";
+import { expCards } from "../../constants";
+import GlowCard from "../components/GlowCard";
 
 const ExperienceSection = () => {
-  return (
-    <section id='experience' className='section-padding w-full md:mt-40 mt-20 xl:px-0'>
-        <div className="w-full h-full md:px-20 px-5">
-            <TitleHeader title='Professional Work Experience' sub='💻 My career overiview'/>
+    return (
+        <section
+            id="experience"
+            className="section-padding w-full md:mt-40 mt-20 xl:px-0"
+        >
+            <div className="w-full h-full md:px-20 px-5">
+                {/* TITLE HEADER */}
+                <TitleHeader
+                    title="Professional Work Experience"
+                    sub="💻 My career overiview"
+                />
 
-            <div className="mt-32 relative">
-                <div className="relative z-50 xl:space-y-32 space-y-10">
-                    {expCards.map((card, index) => (
-                        <div key={card.title} className='exp-card-wrapper'>
-                            <div className="xl:w-2/6">
-                                <GlowCard card={card} index={index} > 
-                                    <div>
-                                        <img src={card.imgPath} alt={card.title} />
+
+                <div className="mt-32 relative">
+                    <div className="relative z-50 xl:space-y-32 space-y-10">
+                        {expCards.map((card, index) => (
+                            <div key={card.title} className="exp-card-wrapper">
+
+                                {/* EXPERIENCE CARDS */}
+                                <div className="xl:w-2/6">
+                                    <GlowCard card={card} index={index}>
+                                        <div>
+                                            <img src={card.imgPath} alt={card.title} />
+                                        </div>
+                                    </GlowCard>
+                                </div>
+
+                                {/* TIMELINE */}
+                                <div className="xl:w-4/6 ">
+                                    <div className="flex items-start">
+                                        <div className="timeline-wrapper">
+                                            <div className="timeline"/>
+                                            <div className="gradient-line w-1 h-full" /> 
+                                        </div>
+
+                                        <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                                            <div className="timeline-logo">
+                                                <img src={card.logoPath} alt="logo"/>
+                                            </div>
+                                            <div>
+                                                <h1 className="font-semibold text-3xl">{card.title}</h1>
+                                                <p className="my-5 text-white-50">📆 {card.date}</p>
+                                                <p className="text-[#839cb5] italic">Responsibilities</p>
+                                                <ul className="list-disc list-inside">
+                                                    {card.responsibilities.map((responsibility) => (
+                                                        <li key={responsibility}>{responsibility}</li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        </div>
                                     </div>
-                                </GlowCard>
+                                </div>
                             </div>
-                        </div>
-                    ))}
 
+
+
+                        ))}
+                    </div>
                 </div>
-
             </div>
-        </div>
-    </section>
-  )
-}
+        </section >
+    );
+};
 
-export default ExperienceSection
+export default ExperienceSection;
