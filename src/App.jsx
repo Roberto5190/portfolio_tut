be46115cfa8382df0177d7a95ed5205e0f1eb1c6
Roberto <rfernandez5190@esdmadrid.es>
2025-04-16 20:29:20 +0200

    added LogoSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Hero from './sections/Hero'
 import ShowCaseSection from './sections/ShowCaseSection'
 import NavBar from './components/NavBar'
+import LogoSection from './components/LogoSection'
+
 function App() {
 
   return (
@@ -8,6 +10,7 @@ function App() {
       <NavBar />
       <Hero />
       <ShowCaseSection />
+      <LogoSection />
     </>
   )
 }
